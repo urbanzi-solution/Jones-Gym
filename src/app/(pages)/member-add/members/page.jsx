762a4b603bca350d18deb1fedcdb99dae16e2f3f
shuhaibtuttushuhaib@gmail.com <shuhaibtuttushuhaib@gmail.com>
2025-07-22 22:06:23 +0530

    dynamic

--- src/app/(pages)/member-add/members/page.jsx
+++ src/app/(pages)/member-add/members/page.jsx
@@ -1,6 +1,8 @@
 // /src/app/members/page.js
 import { query } from '@/lib/db'
 
+export const dynamic = 'force-dynamic'
+
 export default async function MembersPage() {
   const { rows } = await query('SELECT * FROM user_data')
   
